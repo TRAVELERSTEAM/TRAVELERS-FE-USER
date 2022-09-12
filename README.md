0ea1e31d388a3c6a863d6c9ce45c0ae57aca070b
wonder-H <soimar@naver.com>
2022-09-12 17:56:20 +0900

    # 프로젝트 기본 설정
    프로젝트 기본 설정

---
+++ README.md
@@ -0,0 +1,29 @@
+# Go Together team4 project
+
+# 실행
+
+```
+  1. yarn 
+  2-1. yarn dev(개발용)
+  2-2. yarn build(배포용)
+```
+
+# 사용 도구들
+
+```
+  "react": "^18.2.0",
+  "react-dom": "^18.2.0",
+  "react-query": "^3.39.2",
+  "recoil": "^0.7.5"
+  "@types/react": "^18.0.17",
+  "@types/react-dom": "^18.0.6",
+  "@vitejs/plugin-react": "^2.1.0",
+  "eslint-config-prettier": "^8.5.0",
+  "eslint-plugin-prettier": "^4.2.1",
+  "prettier": "^2.7.1",
+  "styled-components": "^5.3.5",
+  "typescript": "^4.6.4",
+  "vite": "^3.1.0"
+```
+
+# local server port: 4000
---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { RecoilRoot } from 'recoil';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Suspense fallback={<p>Loading</p>}>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <ReactQueryDevtools initialIsOpen={true} />
+          <App />
+        </RecoilRoot>
+      </QueryClientProvider>
+    </Suspense>
+  </React.StrictMode>,
+);
