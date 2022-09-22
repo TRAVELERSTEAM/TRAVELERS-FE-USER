015c9031ae1dd30e19dd084bee14afefa6c5739c
wonder-H <soimar@naver.com>
2022-09-22 22:38:09 +0900

    conf: 환경설정변경/#6

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tsconfigPaths()],
+  plugins: [react(), tsconfigPaths()],
   server: {
-    port: 4000
-  }
-})
+    port: 4000,
+  },
+});
