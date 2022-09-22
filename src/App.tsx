37762035045f320567570aec02ee4a732fead65d
wonder-H <soimar@naver.com>
2022-09-22 16:02:49 +0900

    feat: Main임시주석, MyPage연결/#6

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import Header from './components/common/Header';
-import Main from './components/common/Main';
+// import Main from './components/common/Main';
 import Footer from './components/common/Footer';
+import MyPage from './pages/MyPage';
 
 function App() {
   return (
     <>
       <Header />
-      <Main />
+      {/* 마이페이지 작업으로 메인 임시 주석 */}
+      {/* <Main /> */}
+      <MyPage></MyPage>
       <Footer />
     </>
   );
