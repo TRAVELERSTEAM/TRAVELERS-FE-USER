4596ab1b5aef956a0cb66712df21489752ae71a1
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-09-26 16:21:27 +0900

    feat: Login페이지 경로 설정/#13

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import Header from './components/common/Header';
 import Main from './components/common/Main';
 import Footer from './components/common/Footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/LoginPage';
 
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </>
   );
