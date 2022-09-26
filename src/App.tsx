508d8029a24229f9faab7b8e307130984e3c1800
Mangrove-Sun <tjsghd0317@gmail.com>
2022-09-26 17:41:16 +0900

    feat: 메인 배너 슬라이드 기능 구현/#11

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Header from './components/common/Header';
 import Main from './components/common/Main';
 import Footer from './components/common/Footer';
+import MainPage from './pages/MainPage';
 
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <MainPage />
       <Footer />
     </>
   );
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,83 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Autoplay, Pagination, Navigation } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+// import Banner01 from '/banner01.svg';
+// import Banner02 from '/banner02.svg';
+// import Banner03 from '/banner03.svg';
+import TypeTest from '/banner_type-test.svg';
+import AiRecommend from '/banner_ai-recommend.svg';
 
 function MainPage() {
-  return <div>MainPage</div>;
+  return (
+    <section>
+      <SlideWrap className="tslide-wrap">
+        <Swiper
+          spaceBetween={0}
+          slidesPerView={1}
+          centeredSlides={true}
+          pagination={{ clickable: true }}
+          loop={true}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: true,
+          }}
+          speed={1000}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <Img src={AiRecommend} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Img src={TypeTest} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Img src={AiRecommend} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Img src={TypeTest} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Img src={AiRecommend} alt="" />
+          </SwiperSlide>
+        </Swiper>
+      </SlideWrap>
+    </section>
+  );
 }
 
 export default MainPage;
+
+const SlideWrap = styled.div`
+  .swiper-button-prev {
+  }
+  .swiper-button-next {
+  }
+  .swiper-pagination {
+    bottom: 53px;
+    .swiper-pagination-bullet {
+      margin-right: 10px;
+      width: 109px;
+      height: 11px;
+      background-color: #fff;
+      border-radius: 5px;
+      opacity: 1;
+      &:last-child {
+        margin-right: 0;
+      }
+    }
+    .swiper-pagination-bullet-active {
+      background-color: #000;
+    }
+  }
+`;
+
+const Img = styled.img`
+  width: 100%;
+`;
