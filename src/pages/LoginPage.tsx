a174bac3b0fef01fdf16c573e193ed462248c4dd
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-09-26 17:35:17 +0900

    design: Login페이지 스타일 작성/#13

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -53,20 +53,64 @@ function Login() {
   };
 
   return (
-    <section>
+    <LoginContainer>
       <h1>로그인</h1>
-      <div>
-        <button onClick={onInput} value={LoginMethod.member} className="member active">
+      <ButtonBox>
+        <LoginMethodButton onClick={onInput} value={LoginMethod.member} className="member active">
           회원
-        </button>
-        <button onClick={onInput} value={LoginMethod.noMember} className="noMember">
+        </LoginMethodButton>
+        <LoginMethodButton onClick={onInput} value={LoginMethod.noMember} className="noMember">
           비회원 예약조회
-        </button>
-      </div>
+        </LoginMethodButton>
+      </ButtonBox>
       {loginMethod === LoginMethod.member && <MemberLogin />}
       {loginMethod === LoginMethod.noMember && <NoMemberLogin />}
-    </section>
+    </LoginContainer>
   );
 }
 
 export default Login;
+
+const LoginContainer = styled.section`
+  width: 668px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  h1 {
+    font-size: 40px;
+  }
+`;
+
+const ButtonBox = styled.div`
+  width: 100%;
+  margin-bottom: 52px;
+`;
+
+const LoginMethodButton = styled.button`
+  width: 50%;
+  font-size: 32px;
+  border: none;
+  outline: none;
+  background-color: transparent;
+  padding: 10px 0;
+  cursor: pointer;
+  margin-bottom: 10px;
+  border: 1px solid #dddddd;
+  border-left-color: transparent;
+  border-right-color: transparent;
+  border-bottom: 1px solid #000;
+  &:hover {
+    color: #0080c6;
+    background-color: rgba(0, 0, 0, 0.05);
+  }
+  &.active {
+    color: #0080c6;
+    border: 1px solid #000;
+    border-bottom-color: transparent;
+    &:hover {
+      background-color: transparent;
+    }
+  }
+`;
