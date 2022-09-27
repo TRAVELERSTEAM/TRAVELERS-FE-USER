c09c7552be5cc954f84899549ac0f2fc7be60a1a
Mangrove-Sun <tjsghd0317@gmail.com>
2022-09-27 16:20:07 +0900

    feat: 실시간인기상품 구조 및 slide 구현/#11

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -12,9 +12,10 @@ import TypeTest from '/banner_type-test.svg';
 import AiRecommend from '/banner_ai-recommend.svg';
 
 function MainPage() {
+  const count = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
   return (
-    <section>
-      <SlideWrap className="tslide-wrap">
+    <Section>
+      <SlideWrap className="slide-wrap">
         <Swiper
           spaceBetween={0}
           slidesPerView={1}
@@ -48,12 +49,111 @@ function MainPage() {
           </SwiperSlide>
         </Swiper>
       </SlideWrap>
-    </section>
+      <article className="group-list">
+        <Inner>
+          <h2>그룹별</h2>
+          <div>
+            <ul>
+              <li>
+                <a href="#">
+                  <img src="" alt="" />
+                  <span>5070끼리</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="" alt="" />
+                  <span>2040끼리</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="" alt="" />
+                  <span>남자끼리</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="" alt="" />
+                  <span>여자끼리</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="" alt="" />
+                  <span>자녀동반</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </Inner>
+      </article>
+      <HotItemsWrap className="hot-items">
+        <Inner>
+          <h2>실시간 인기 상품</h2>
+          <SlideWrap>
+            <Swiper
+              spaceBetween={20}
+              slidesPerView={3}
+              slidesPerGroup={3}
+              loop={true}
+              loopFillGroupWithBlank={true}
+              navigation={true}
+              modules={[Navigation]}
+              speed={1000}
+            >
+              {count.map((_, index) => {
+                return (
+                  <SwiperSlide className="item">
+                    <a href="#">
+                      <div className="thumb">
+                        <img src="" alt="" />
+                        <span>{index + 1}</span>
+                      </div>
+                      <div className="desc">
+                        <span className="title">타이틀타이틀타이틀타이틀타이틀</span>
+                        <span className="price">{'1,000,000'} 원</span>
+                      </div>
+                    </a>
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </SlideWrap>
+        </Inner>
+      </HotItemsWrap>
+    </Section>
   );
 }
 
 export default MainPage;
 
+const Section = styled.section`
+  * {
+    text-decoration: none;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+  a {
+    color: #000;
+  }
+  width: 100%;
+  span {
+    font-size: 16px;
+  }
+`;
+
+const Inner = styled.div`
+  margin: 0 auto;
+  width: 1360px;
+  h2 {
+    margin-bottom: 41px;
+    font-weight: 700;
+    font-size: 55px;
+    line-height: 67px;
+  }
+`;
 const SlideWrap = styled.div`
   .swiper-button-prev {
   }
@@ -80,4 +180,38 @@ const SlideWrap = styled.div`
 
 const Img = styled.img`
   width: 100%;
+`;
+
+const HotItemsWrap = styled.article`
+  margin-bottom: 240px;
+  .item {
+    a {
+      .thumb {
+        width: 440px;
+        height: 291px;
+        background-color: #ddd;
+        span {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          font-size: 20px;
+        }
+      }
+      .desc {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 11px 26px;
+        span {
+          font-weight: 600;
+          font-size: 40px;
+          line-height: 48px;
+          text-align: center;
+        }
+        .title {
+          margin-bottom: 30px;
+        }
+      }
+    }
+  }
 `;
