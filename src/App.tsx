6f421231a60d4520c971111651f1ace93249aa04
wonder-H <soimar@naver.com>
2022-09-27 16:33:56 +0900

    feat: 메인브랜치 적용 전 라우트 경로 설정/#7

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import Footer from '~/components/common/Footer';
 import MyPage from '~/pages/MyPage';
 import NotfoundPage from '~/pages/NotfoundPage';
 import ProductDetailPage from '~/pages/ProductDetailPage';
+import DeleteAccount from './components/MyPage/DeleteAccount';
+import WishList from './components/MyPage/WishList';
+import Reservation from './components/MyPage/Reservation';
 
 function App() {
   return (
@@ -12,9 +15,38 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
+          {/* 메인 */}
           <Route index element={<Main />} />
-          {/* <ProductDetailPage /> */}
-          <Route path="/mypage" element={<MyPage />} />
+          {/* 그룹별여행 */}
+          <Route path="groups" element={<Groups />} />
+          {/* 지역별여행 */}
+          <Route path="destination" element={<Destination />} />
+          {/* 테마별여행  */}
+          <Route path="themes" element={<Themes />} />
+          {/* 상품상세페이지 */}
+          <Route path="product_detail/:id" element={<ProductDetailPage />} />
+          {/* 아이디/비밀번호 찾기 페이지 */}
+          <Route path="find" element={<Find />} />
+          {/* 마이페이지 */}
+          <Route path="mypage" element={<MyPage />}>
+            {/* -내정보 - 정보수정 - 회원탈퇴 - 찜목록 - 예약 및 취소 조회 - 1:1문의 */}
+            <Route path="myinfo" element={<MyInfo />} />
+            <Route path="editinfo" element={<EditInfo />} />
+            <Route path="delete_account" element={<DeleteAccount />} />
+            <Route path="wishlist" element={<WishList />} />
+            <Route path="reservation" element={<Reservation />} />
+            <Route path="inquiry" element={<Inquiry />} />
+          </Route>
+          {/* 로그인 */}
+          <Route path="login" element={<Login />} />
+          {/* 공지사항 */}
+          <Route path="notice" element={<Notice />} />
+          {/* 회원가입 */}
+          <Route path="signup" element={<SignUp />} />
+          {/* 회원가입 완료 페이지 */}
+          <Route path="signupsuccess" element={<SignUpSuccess />} />
+          {/* 자료실 */}
+          <Route path="reference" element={<Reference />} />
           <Route path="*" element={<NotfoundPage />} />
         </Routes>
         <Footer />
